02ed1f3de368e1f877d9f333bbedd84b562c5287
Roman Sterlin <Feuse132@gmail.com>
2021-08-17 17:17:50 +0300

    Add project files.

---
+++ Utills/test.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Services.Server.Utills
+{
+    public struct test<Tkey,Tvalue>
+    {
+        public Tkey one;
+        public Tvalue two;
+
+        public test(Tkey one, Tvalue two )
+        {
+            this.one = one;
+            this.two = two;
+        }
+    }
+}
